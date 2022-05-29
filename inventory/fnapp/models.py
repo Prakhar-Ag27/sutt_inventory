36c4c67115edc2723db7c6044a8458d4feb91e94
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2022-05-29 15:22:03 +0530

    updated after running migrations

--- inventory/fnapp/models.py
+++ inventory/fnapp/models.py
@@ -8,7 +8,7 @@ class items(models.Model):
     qty_available = models.IntegerField(default = 0)
     last_issued = models.DateTimeField()
     no_of_items_issued_out = models.IntegerField(default = 0)
-    unique_code = models.IntegerField(max_length=100, default = random.randint(10_000_000, 99_999_999), editable = False)
+    unique_code = models.IntegerField(default = random.randint(10_000_000, 99_999_999), editable = False)
     
 
     def __str__(self):
