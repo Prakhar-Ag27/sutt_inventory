d1bde577468184038c490155bccf677104311bcd
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2022-06-02 21:54:50 +0530

    completed return item functionality but the problem is if the user issues same item twice with same unique code then the logic breaks. Need to amend.

--- inventory/fnapp/urls.py
+++ inventory/fnapp/urls.py
@@ -6,8 +6,10 @@ app_name = 'fnapp'
 urlpatterns = [
     path('', views.details, name='details'),
     path('<int:itemcode>/', views.issue, name='issue'),
-    path('success/<int:itemcode>/', views.success, name='success'),
+    path('success/<int:itemcode>/<int:temp>', views.success_return, name='success_return'),
+    path('success/<int:itemcode>/', views.success_issue, name='success_issue'),
     path('logadd/', views.log_add, name='logadd'),
     path('logissue/', views.log_issue, name='logissue'),
     path('logreturn/', views.log_return, name='logreturn'),
+    path('returnitems/', views.returnitems, name='returnitems'),
 ]
--- inventory/fnapp/views.py
+++ inventory/fnapp/views.py
@@ -14,7 +14,7 @@ def details(request):
         obj.save()
 
     item_list = Item.objects.all()
-    context = {'item_list': item_list}
+    context = {'item_list': item_list, 'type' : "issue"}
     if request.user.is_authenticated:
         return render(request, 'fnapp/detailspage.html', context)
         
@@ -29,7 +29,15 @@ def issue(request, itemcode):
     else:
         return render(request, 'fnapp/notlogin.html')
 
-def success(request, itemcode):
+def returnitems(request):
+    item_list = Through.objects.filter(student__email_id = request.user.email).order_by('-time_of_return')
+    context = {'item_list': item_list, 'type' : "return"}
+    if request.user.is_authenticated:
+        return render(request, 'fnapp/detailspage.html', context)
+    else:
+        return render(request, 'fnapp/notlogin.html')
+
+def success_issue(request, itemcode):
     if request.user.is_authenticated:
         if request.user.is_superuser:
             pass
@@ -39,29 +47,71 @@ def success(request, itemcode):
             obj.student = Student.objects.get(email_id=request.user.email)
             obj.qty_issued = int(request.POST['items'])
             obj.save()
-        print(int(request.POST['items']))
-        item_issued = Item.objects.get(unique_code = itemcode)
-        item_issued.total_qty = item_issued.total_qty - int(request.POST['items'])
-        item_issued.current_out = item_issued.current_out + int(request.POST['items'])
-        item_issued.save()
+            item_issued = Item.objects.get(unique_code = itemcode)
+            item_issued.total_qty = item_issued.total_qty - int(request.POST['items'])
+            item_issued.current_out = item_issued.current_out + int(request.POST['items'])
+            item_issued.save()
 
-        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-            smtp.ehlo()
-            smtp.starttls()
-            smtp.ehlo()
-            smtp.login("agrawal.prakhar35@gmail.com", "tndumdixwmkppymx")
-            subject = 'Regarding issue of item from IMS'
-            body = f'You have successfully issued {Item.name_of_item} with item code {Item.unique_code}.'
-            msg = f'Subject: {subject}\n\n{body}'
-            smtp.sendmail("agrawal.prakhar35@gmail.com", request.user.email, msg)
+            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
+                smtp.login("agrawal.prakhar35@gmail.com", "tndumdixwmkppymx")
+                subject = 'Regarding issue of item from IMS'
+                body = f'You have successfully issued {item_issued.name_of_item} with item code {item_issued.unique_code}.'
+                msg = f'Subject: {subject}\n\n{body}'
+                smtp.sendmail("agrawal.prakhar35@gmail.com", request.user.email, msg)
 
-        return render(request, 'fnapp/successpage.html', {"itemcode" : itemcode})
+                return render(request, 'fnapp/successpage_issue.html', {"itemcode" : itemcode})
+
+    else:
+        return render(request, 'fnapp/notlogin.html')
+
+def success_return(request, itemcode, temp):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            pass
+        else:
+            obj = Through.objects.get(item__unique_code = itemcode)
+            if temp == 1:
+                obj.qty_issued = obj.qty_returned
+                obj.item.total_qty = obj.item.total_qty + obj.item.current_out
+                obj.item.current_out = 0
+                obj.save()
+                with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                    smtp.ehlo()
+                    smtp.starttls()
+                    smtp.ehlo()
+                    smtp.login("agrawal.prakhar35@gmail.com", "tndumdixwmkppymx")
+                    subject = 'Regarding return of issued item from IMS'
+                    body = f'You have successfully returned {obj.item.name_of_item} with item code {obj.item.unique_code}.'
+                    msg = f'Subject: {subject}\n\n{body}'
+                    smtp.sendmail("agrawal.prakhar35@gmail.com", request.user.email, msg)
+
+                    return render(request, 'fnapp/successpage_return.html', {"itemcode" : itemcode})
+
+            if temp == 0:
+                obj.qty_returned = int(request.POST['items'])
+                obj.item.total_qty = obj.item.total_qty + obj.qty_returned
+                obj.item.current_out = obj.item.current_out - obj.qty_returned
+                obj.save()
+                with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                    smtp.ehlo()
+                    smtp.starttls()
+                    smtp.ehlo()
+                    smtp.login("agrawal.prakhar35@gmail.com", "tndumdixwmkppymx")
+                    subject = 'Regarding issue of item from IMS'
+                    body = f'You have successfully returned {obj.qty_returned} out of {obj.qty_issued} issued {obj.item.name_of_item} with item code {obj.item.unique_code}'
+                    msg = f'Subject: {subject}\n\n{body}'
+                    smtp.sendmail("agrawal.prakhar35@gmail.com", request.user.email, msg)
+
+                    return render(request, 'fnapp/successpage_return.html', {"itemcode" : itemcode})
 
     else:
         return render(request, 'fnapp/notlogin.html')
 
 def log_add(request):
-    logitems = Item.objects.all().order_by(-'added_on')
+    logitems = Item.objects.all().order_by('-added_on')
     context = {'logitems': logitems}
     if request.user.is_authenticated:
         return render(request, 'fnapp/log_add.html', context)
@@ -69,7 +119,7 @@ def log_add(request):
         return render(request, 'fnapp/notlogin.html')
 
 def log_issue(request):
-    logitems = Through.objects.all().order_by(-'time_of_issue')
+    logitems = Through.objects.all().order_by('-time_of_issue')
     context = {'logitems': logitems}
     if request.user.is_authenticated:
         return render(request, 'fnapp/log_issue.html', context)
@@ -77,11 +127,13 @@ def log_issue(request):
         return render(request, 'fnapp/notlogin.html')
 
 def log_return(request):
-    logitems = Through.objects.all().order_by(-'time_of_return')
+    logitems = Through.objects.all().order_by('-time_of_return')
     context = {'logitems': logitems}
     if request.user.is_authenticated:
         return render(request, 'fnapp/log_return.html', context)
     else:
         return render(request, 'fnapp/notlogin.html')
+
+
     
 
